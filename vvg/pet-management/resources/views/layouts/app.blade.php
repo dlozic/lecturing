6d5270c9207bd7c5c97f1505df006dea226ebea2
Davor Lozic <davor@lozic.dev>
2024-01-03 22:26:15 +0100

    vvg: Showcase errors globally in pet-management

--- vvg/pet-management/resources/views/layouts/app.blade.php
+++ vvg/pet-management/resources/views/layouts/app.blade.php
@@ -1,21 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>PetManagement</title>
-        <link
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>PetManagement</title>
+    <link
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
             rel="stylesheet"
             integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
             crossorigin="anonymous"
         />
-    </head>
-    <body>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="/">PetManagement</a>
-                <button
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/">PetManagement</a>
+            <button
                     class="navbar-toggler"
                     type="button"
                     data-bs-toggle="collapse"
@@ -24,47 +25,58 @@
                     aria-expanded="false"
                     aria-label="Toggle navigation"
                 >
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div
                     class="collapse navbar-collapse"
                     id="navbarSupportedContent"
                 >
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('users.index') }}">Users</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('tags.index') }}">Tags</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('breeds.index') }}">Breeds</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('pets.index') }}">Pets</a>
-                        </li>
-                    </ul>
-                    <ul class="navbar-nav mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('logout') }}">Logout</a>
-                        </li>
-                    </ul>
-                </div>
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('users.index') }}">Users</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('tags.index') }}">Tags</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('breeds.index') }}">Breeds</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('pets.index') }}">Pets</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('logout') }}">Logout</a>
+                    </li>
+                </ul>
             </div>
-        </nav>
-        <div class="container p-5">
-            @if(session('success'))
-            <div class="alert alert-success">{{ session('success') }}</div>
-            @endif
-            @if(session('error'))
-            <div class="alert alert-danger">{{ session('error') }}</div>
-            @endif
         </div>
-        <div class="container p-5">@yield('content')</div>
-        <script
+    </nav>
+    <div class="container p-5">
+        @if(session('success'))
+        <div class="alert alert-success">{{ session('success') }}</div>
+        @endif
+        @if(session('error'))
+        <div class="alert alert-danger">{{ session('error') }}</div>
+        @endif
+        
+        @if ($errors->any())
+        <!-- error list, if any -->
+        <div class="alert alert-danger col-lg-3 mx-auto mt-5">
+            <ul class="list-unstyled m-0">
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
+    </div>
+    <div class="container p-5">@yield('content')</div>
+    <script
             src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
             crossorigin="anonymous"
         ></script>
-    </body>
+</body>
 </html>
